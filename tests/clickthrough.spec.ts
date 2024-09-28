ba8bfd57d5d3e2dcb88c4481cc5592a44a72c463
Igor Shevchenko <cyberskunk@gmail.com>
2024-09-28 22:51:21 +0300

    Add test to click through the main scenarios

---
+++ tests/clickthrough.spec.ts
@@ -0,0 +1,79 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  /* Start quiz */
+  await page.getByRole('button', { name: 'Start Quiz' }).click();
+
+  /* Answer 5 questions */
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  await page.getByRole('button').nth(4).click();
+  
+  /* Finish quiz */
+  await page.getByRole('button', { name: 'Finish Quiz' }).click();
+
+  /* Start next round */
+  await page.getByRole('button', { name: 'Start Next Round' }).click();
+
+  /* Answer 5 questions */
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  await page.getByRole('button').nth(4).click();
+
+  /* Finish quiz */
+  await page.getByRole('button', { name: 'Finish Quiz' }).click();
+
+  /* Reset quiz */
+  await page.getByRole('button', { name: 'Reset Quiz' }).click();
+
+  /* Start quiz */
+  await page.getByRole('button', { name: 'Start Quiz' }).click();
+
+  /* Answer 1 question */
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  /* click x button and confirm */
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: '×' }).click();
+
+  /* Answer 1 question */
+  await page.getByRole('button').nth(4).click();
+  await page.getByRole('button', { name: 'Next Question' }).click();
+
+  /* click x button and confirm */
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.accept().catch(() => {});
+  });
+  await page.getByRole('button', { name: '×' }).click();
+
+  /* we are at the beginning again */
+  // expect(await page.getByRole('button', { name: 'Start Quiz' })).toBeInViewport();
+  await page.getByRole('button', { name: 'Start Quiz' }).click();
+});
